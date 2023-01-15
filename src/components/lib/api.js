09ab1698e3188fbe574291c019041e671b6ad9b7
Alii_ <33094586+Popeyee@users.noreply.github.com>
2023-01-15 16:14:05 -0500

    initial commit with router v5

---
+++ src/components/lib/api.js
@@ -0,0 +1,97 @@
+const FIREBASE_DOMAIN = 'https://react-http-6c9e0-default-rtdb.firebaseio.com';
+
+export const getAllQuotes =async () => {
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes/quotes.json`);
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not fetch quotes.');
+  }
+
+  const transformedQuotes = [];
+
+  for (const key in data) {
+    const quoteObj = {
+      id: key,
+      ...data[key],
+    };
+
+    transformedQuotes.push(quoteObj);
+  }
+console.log('TQ',transformedQuotes)
+  return transformedQuotes;
+  
+}
+
+export async function getSingleQuote(quoteId) {
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes/quotes/${quoteId}.json`);
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not fetch quote.');
+  }
+
+  const loadedQuote = {
+    id: quoteId,
+    ...data,
+  };
+
+  return loadedQuote;
+}
+
+export const addQuote = async (quoteData) => {
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes/quotes.json`, {
+    method: 'POST',
+    body: JSON.stringify(quoteData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not create quote.');
+  }
+
+  return null;
+}
+
+export async function addComment(requestData) {
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes/comments/${requestData.quoteId}.json`, {
+    method: 'POST',
+    body: JSON.stringify(requestData.commentData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not add comment.');
+  }
+
+  return { commentId: data.name };
+}
+
+export async function getAllComments(quoteId) {
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes/comments/${quoteId}.json`);
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not get comments.');
+  }
+
+  const transformedComments = [];
+
+  for (const key in data) {
+    const commentObj = {
+      id: key,
+      ...data[key],
+    };
+
+    transformedComments.push(commentObj);
+  }
+
+  return transformedComments;
+}
---
+++ src/components/pages/notFound.js
@@ -0,0 +1,14 @@
+
+
+
+const NotFound = () => {
+    return (
+        <div className="centered">
+            <p>Page not found!</p>
+        </div>
+    )
+};
+
+
+
+export default NotFound;
